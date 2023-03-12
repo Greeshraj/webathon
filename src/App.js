8dd5b68278a5ff156ce37c6f61b0d4757c045a35
Greeshraj <greeshraj@kgpian.iitkgp.ac.in>
2023-03-12 20:05:04 +0530

    Frontend done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 // import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
-
+import Nav from './components/nav';
+import Faq from './components/faq';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Review from './components/review';
+import Demo from './components/demo';
+import Courses from './components/courses';
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    // <div className="App">
+    //   <Nav/>
+    //   <Faq/>
+    //   {/* <Home/> */}
+    //   <Route exact path='/nav' component={Nav} />
+
+    // </div>
+  <>
+  <Nav/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+          {/* <Route index element={<Home />} /> */}
+          {/* <Route path="/nav" element={<Nav />} /> */}
+          <Route path="/faq" element={< Faq/>} />
+          <Route path="/reviews" element={< Review/>} />
+          <Route path="/demo" element={< Demo/>} />
+          <Route path="/courses" element={< Courses/>} />
+        {/* </Route> */}
+      </Routes>
+    </BrowserRouter></>
   );
 }
 
